5382e2ce25eaea0c2aff8fe7491996a4e8437c13
ibis <ignaciabaeza.i@gmail.com>
2025-11-18 12:37:18 -0300

    frontend fix

--- drimboard_backend/app/app.py
+++ drimboard_backend/app/app.py
@@ -28,21 +28,20 @@ app = FastAPI()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 ENV = os.getenv("FLASK_ENV")
 
-# --- CORS Configuration ---
+
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://develop.d17k1lr65yqfqv.amplifyapp.com/"
+    "https://develop.d17k1lr65yqfqv.amplifyapp.com"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=origins,  # NOT ["*"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 # --- Configuration ---
 SECRET_KEY = os.getenv("JWT_SECRET")
 if not SECRET_KEY:
--- drimboard_frontend/next.config.mjs
+++ drimboard_frontend/next.config.mjs
@@ -1,14 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5001';
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
       {
         source: '/:path*', // This will match any path starting with /api
-        destination: 'http://localhost:5001/:path*', // Proxy to your Flask backend
+        destination: `${API_URL}/:path*`
       },
     ];
   },
 };
 
 export default nextConfig;
+
